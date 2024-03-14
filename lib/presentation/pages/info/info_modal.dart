b77f2ce2da2d5045acdeafaf837e8d938fdb8fa7
Augustob790 <augustosousa790@gmail.com>
2024-03-14 11:56:23 -0300

    fix: small fixed

--- lib/presentation/pages/info/info_modal.dart
+++ lib/presentation/pages/info/info_modal.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:crud_flutter/presentation/pages/widgets/input_personalized.dart';
 import 'package:crud_flutter/presentation/pages/widgets/mask.dart';
 
 import '../../controller/home_page_controller.dart';
@@ -42,57 +41,64 @@ class _InfoPeriodState extends State<InfoPeriod> {
   _buildAddProductsModal(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return AlertDialog(
-      backgroundColor: Colors.white,
-      content: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 60.0),
-                        child: const Text(
-                          "Novo Período",
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w500,
-                            color: Color.fromRGBO(46, 44, 52, 1),
+      scrollable: true,
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      contentPadding: const EdgeInsets.all(0),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+          width: size.width,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 255, 255, 255),
+              border: Border.all(
+                color: Color.fromARGB(180, 205, 205, 205),
+                width: 1,
+              ),
+              borderRadius: const BorderRadius.all(Radius.circular(20)),
+            ),
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 80.0),
+                      child: const Text(
+                        "Novo Período",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color: Color.fromRGBO(46, 44, 52, 1),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 5.0),
+                          child: IconButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            icon: Icon(Icons.close),
+                            color: Colors.grey,
                           ),
                         ),
                       ),
-                      Expanded(
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 5.0),
-                            child: TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: Manrope(
-                                  text: "X",
-                                  size: 20,
-                                  color:
-                                      const Color.fromARGB(255, 188, 188, 188),
-                                )),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 5),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
+                    )
+                  ],
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(
+                      child: Container(
                         height: 45,
-                        width: (size.width / 1.74),
                         decoration: BoxDecoration(
                           color: Color.fromARGB(27, 66, 66, 67),
                           border: Border.symmetric(),
@@ -101,7 +107,7 @@ class _InfoPeriodState extends State<InfoPeriod> {
                         ),
                         child: Padding(
                           padding: const EdgeInsets.all(10.0),
-                          child:  Manrope(
+                          child: Manrope(
                             text: widget.controller.titleController.text,
                             color: Color.fromARGB(255, 12, 11, 11),
                             font: FontWeight.w500,
@@ -109,156 +115,154 @@ class _InfoPeriodState extends State<InfoPeriod> {
                           ),
                         ),
                       ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 5),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Manrope(
+                            text: "Começa",
+                            color: Color.fromARGB(255, 12, 11, 11),
+                            font: FontWeight.w500,
+                            size: 14,
+                          ),
+                          const SizedBox(width: 5),
+                          Manrope(
+                            text: Mask.formatDateForBR2(widget.dateInit),
+                            color: Color.fromARGB(255, 12, 11, 11),
+                            font: FontWeight.w400,
+                            size: 14,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 2.0, right: 2.0),
+                      child: Divider(height: 10),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10, bottom: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Manrope(
+                            text: "Termina",
+                            color: Color.fromARGB(255, 12, 11, 11),
+                            font: FontWeight.w500,
+                            size: 14,
+                          ),
+                          const SizedBox(width: 5),
+                          Manrope(
+                            text: Mask.formatDateForBR2(widget.dateFinal),
+                            color: Color.fromARGB(255, 12, 11, 11),
+                            font: FontWeight.w400,
+                            size: 14,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 2.0, right: 2.0),
+                      child: Divider(height: 10),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10, bottom: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Manrope(
+                            text: "Categoria",
+                            color: Color.fromARGB(255, 12, 11, 11),
+                            font: FontWeight.w500,
+                            size: 14,
+                          ),
+                          const SizedBox(width: 5),
+                          Manrope(
+                            text: widget.category,
+                            color: Color.fromARGB(255, 12, 11, 11),
+                            font: FontWeight.w400,
+                            size: 14,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Manrope(
+                          text: "Meta 1",
+                          color: Color.fromARGB(255, 12, 11, 11),
+                          font: FontWeight.w500,
+                          size: 14,
+                        ),
+                        const SizedBox(width: 5),
+                        Manrope(
+                          text: widget.meta1,
+                          color: Color.fromARGB(255, 12, 11, 11),
+                          font: FontWeight.w400,
+                          size: 14,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Manrope(
+                          text: "Meta 2",
+                          color: Color.fromARGB(255, 12, 11, 11),
+                          font: FontWeight.w500,
+                          size: 14,
+                        ),
+                        const SizedBox(width: 5),
+                        Manrope(
+                          text: widget.meta2,
+                          color: Color.fromARGB(255, 12, 11, 11),
+                          font: FontWeight.w400,
+                          size: 14,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 25),
+                Padding(
+                  padding: const EdgeInsets.symmetric(),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CustomButtonStandard(
+                        height: 35,
+                        width: 100,
+                        onTap: widget.excluir,
+                        color: Color.fromARGB(255, 255, 0, 0),
+                        text: "Excluir",
+                        isLoading: true,
+                      ),
+                      CustomButtonStandard(
+                        height: 35,
+                        width: 100,
+                        onTap: widget.editar,
+                        color: Color.fromARGB(247, 15, 40, 139),
+                        text: "Editar",
+                        isLoading: true,
+                      ),
                     ],
                   ),
-                  const SizedBox(height: 10),
-                  SizedBox(
-                    width: 210,
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 5),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10, bottom: 10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Manrope(
-                                text: "Começa",
-                                color: Color.fromARGB(255, 12, 11, 11),
-                                font: FontWeight.w500,
-                                size: 14,
-                              ),
-                              const SizedBox(width: 5),
-                              Manrope(
-                                text: Mask.formatDateForBR2(widget.dateInit),
-                                color: Color.fromARGB(255, 12, 11, 11),
-                                font: FontWeight.w400,
-                                size: 14,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 2.0, right: 2.0),
-                          child: Divider(height: 10),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10, bottom: 10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Manrope(
-                                text: "Termina",
-                                color: Color.fromARGB(255, 12, 11, 11),
-                                font: FontWeight.w500,
-                                size: 14,
-                              ),
-                              const SizedBox(width: 5),
-                              Manrope(
-                                text: Mask.formatDateForBR2(widget.dateFinal),
-                                color: Color.fromARGB(255, 12, 11, 11),
-                                font: FontWeight.w400,
-                                size: 14,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 2.0, right: 2.0),
-                          child: Divider(height: 10),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10, bottom: 10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Manrope(
-                                text: "Categoria",
-                                color: Color.fromARGB(255, 12, 11, 11),
-                                font: FontWeight.w500,
-                                size: 14,
-                              ),
-                              const SizedBox(width: 5),
-                              Manrope(
-                                text: widget.category,
-                                color: Color.fromARGB(255, 12, 11, 11),
-                                font: FontWeight.w400,
-                                size: 14,
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Manrope(
-                              text: "Meta 1",
-                              color: Color.fromARGB(255, 12, 11, 11),
-                              font: FontWeight.w500,
-                              size: 14,
-                            ),
-                            const SizedBox(width: 5),
-                            Manrope(
-                              text: widget.meta1,
-                              color: Color.fromARGB(255, 12, 11, 11),
-                              font: FontWeight.w400,
-                              size: 14,
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 20),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Manrope(
-                              text: "Meta 2",
-                              color: Color.fromARGB(255, 12, 11, 11),
-                              font: FontWeight.w500,
-                              size: 14,
-                            ),
-                            const SizedBox(width: 5),
-                            Manrope(
-                              text: widget.meta2,
-                              color: Color.fromARGB(255, 12, 11, 11),
-                              font: FontWeight.w400,
-                              size: 14,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 25),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        CustomButtonStandard(
-                          height: 35,
-                          width: 100,
-                          onTap: widget.excluir,
-                          color: Color.fromARGB(255, 255, 0, 0),
-                          text: "Excluir",
-                          isLoading: true,
-                        ),
-                        CustomButtonStandard(
-                          height: 35,
-                          width: 100,
-                          onTap: widget.editar,
-                          color: Color.fromARGB(247, 15, 40, 139),
-                          text: "Editar",
-                          isLoading: true,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
